0ecb4f2ba7ac40dc69afe6511cd935b624337a96
Steve Maniak <steve.maniak@bradyplc.com>
2016-01-27 13:01:23 -0500

    Terminal Master domain object (1st draft)

---
+++ src/Brady.ScrapRunner.Server/Brady.ScrapRunner.DataService/Validators/TerminalMasterDeletionValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BWF.DataServices.Support.NHibernate.Interfaces;
+using FluentValidation;
+using Brady.ScrapRunner.Domain.Models;
+
+namespace Brady.ScrapRunner.DataService.Validators
+{
+    public class TerminalMasterDeletionValidator : AbstractValidator<TerminalMaster>,
+        IRequireCrudingDataServiceRepository
+    {
+        ICrudingDataServiceRepository _repository;
+
+        public TerminalMasterDeletionValidator()
+        {
+            RuleFor(x => x.TerminalId).NotEmpty();
+        }
+
+        public void SetRepository(ICrudingDataServiceRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
