3677f471920dc2efd010974ff3ca4dc8122914ef
Steve Maniak <steve.maniak@bradyplc.com>
2016-01-29 17:10:23 -0500

    ContainerMaster, ErrorLog, and EverntLog (first draft)

---
+++ src/Brady.ScrapRunner.Domain/Models/EventLog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BWF.DataServices.Metadata.Interfaces;
+
+namespace Brady.ScrapRunner.Domain.Models
+{
+    /// <summary>
+    /// A EventLog record.
+    /// </summary>
+    public class EventLog : IHaveCompositeId, IEquatable<EventLog>
+    {
+        public virtual DateTime EventDateTime { get; set; }
+        public virtual int EventSeqNo { get; set; }
+        public virtual string EventTerminalId { get; set; }
+        public virtual string EventRegionId { get; set; }
+        public virtual string EventEmployeeId { get; set; }
+        public virtual string EventEmployeeName { get; set; }
+        public virtual string EventTripNumber { get; set; }
+        public virtual string EventProgram { get; set; }
+        public virtual string EventScreen { get; set; }
+        public virtual string EventAction { get; set; }
+        public virtual string EventComment { get; set; }
+
+        public virtual string Id
+        {
+            get
+            {
+                //return string.Format("{0};{1}", EventDateTime, EventSeqNo);
+                // "s" is sortable --> 2009-06-15T13:45:30
+                return EventDateTime.ToString("s") + ";" + EventSeqNo;
+            }
+            set
+            {
+
+            }
+        }
+
+        public virtual bool Equals(EventLog other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return DateTime.Equals(EventDateTime, other.EventDateTime) 
+                && string.Equals(EventSeqNo, other.EventSeqNo);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((EventLog) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode =  (EventDateTime != null ? EventDateTime.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ EventSeqNo.GetHashCode() ;
+                return hashCode;
+            }
+        }
+    }
+
+}
---
+++ src/Brady.ScrapRunner.Server/Brady.ScrapRunner.DataService/RecordTypes/EventLogRecordType.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Brady.ScrapRunner.DataService.Validators;
+using Brady.ScrapRunner.Domain.Models;
+using BWF.DataServices.Core.Abstract;
+using BWF.DataServices.Core.Concrete.ChangeSets;
+using BWF.DataServices.Metadata;
+using BWF.DataServices.Metadata.Attributes.Actions;
+using BWF.DataServices.Support.NHibernate.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brady.ScrapRunner.DataService.RecordTypes
+{
+
+    [CreateAction("EventLog")]
+    [EditAction("EventLog")]
+    [DeleteAction("EventLog")]
+    public class EventLogRecordType :
+        ChangeableRecordType<EventLog, long, string, EventLogValidator, EventLogDeletionValidator>
+    {
+        public override void ConfigureMapper()
+        {
+            var mapping = Mapper.CreateMap<EventLog, EventLog>();
+        }
+
+        public override EventLog GetIdentityObject(string id)
+        {
+            var identityValues = TypeMetadataInternal.GetIdentityValues(id);
+            return new EventLog
+            {
+                EventDateTime = DateTime.Parse(identityValues[0]),
+                EventSeqNo = int.Parse(identityValues[1])
+            };
+        }
+
+        public override Expression<Func<EventLog, bool>> GetIdentityPredicate(EventLog item)
+        {
+            return x => x.EventDateTime == item.EventDateTime &&
+                        x.EventSeqNo == item.EventSeqNo;
+        }
+
+        public override Expression<Func<EventLog, bool>> GetIdentityPredicate(string id)
+        {
+            var identityValues = TypeMetadataInternal.GetIdentityValues(id);
+            return x => x.EventDateTime == DateTime.Parse(identityValues[0]) &&
+                        x.EventSeqNo == int.Parse(identityValues[1]);
+        }
+
+    }
+}
