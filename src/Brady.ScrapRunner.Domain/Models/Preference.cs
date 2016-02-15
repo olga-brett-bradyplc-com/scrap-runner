2804ff7ea20dcfe10cb438a4d0513a02ca23cc64
Steve Maniak <steve.maniak@bradyplc.com>
2016-02-15 15:02:09 -0500

    Revert "Reorder compound key of Preference object."
    This reverts commit ef674944fffcc73820aa876c76bda77755958967.

--- src/Brady.ScrapRunner.Domain/Models/Preference.cs
+++ src/Brady.ScrapRunner.Domain/Models/Preference.cs
@@ -18,7 +18,7 @@ namespace Brady.ScrapRunner.Domain.Models
         {
             get
             {
-                return string.Format("{0};{1}", TerminalId, Parameter);
+                return string.Format("{0};{1}", Parameter, TerminalId);
             }
             set
             {
