c9c380f488767f08648b72bb2d5a7cb6f18ec909
jaywhitlow <jay.whitlow@bradyplc.com>
2016-02-18 12:48:53 -0500

    Changes to Scale Detail & Summary View Models

--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/ViewModels/ScaleDetailViewModel.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/ViewModels/ScaleDetailViewModel.cs
@@ -1,4 +1,10 @@
-﻿namespace Brady.ScrapRunner.Mobile.ViewModels
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Brady.ScrapRunner.Mobile.Helpers;
+using Brady.ScrapRunner.Mobile.Interfaces;
+
+namespace Brady.ScrapRunner.Mobile.ViewModels
 {
     using System;
     using Acr.UserDialogs;
@@ -8,10 +14,17 @@
 
     public class ScaleDetailViewModel : BaseViewModel
     {
-        public ScaleDetailViewModel()
+        private readonly IRepository<TripSegmentModel> _tripSegmentRepository;
+        private readonly IRepository<TripSegmentContainerModel> _tripSegmentContainerRepository;
+
+        public ScaleDetailViewModel(
+            IRepository<TripSegmentModel> tripSegmentRepository,
+            IRepository<TripSegmentContainerModel> tripSegmentContainerRepository)
         {
-            Title = "Yard/Scale";
-            Container = CreateDummyData();
+            _tripSegmentRepository = tripSegmentRepository;
+            _tripSegmentContainerRepository = tripSegmentContainerRepository;
+
+            Title = "Yard/Scale Detail";
 
             GrossWeightSetCommand = new MvxCommand(ExecuteGrossWeightSetCommand);
             SecondGrossWeightSetCommand = new MvxCommand(ExecuteSecondGrossWeightSetCommand, IsGrossWeightSet);
@@ -21,9 +34,68 @@
             ContainerLeftOnTruckCommand = new MvxCommand(ExecuteContainerLeftOnTruckCommand);
         }
 
+        public void Init(string tripNumber, string tripSegNumber, string tripSegContainerNumber)
+        {
+            TripNumber = tripNumber;
+            TripSegNumber = tripSegNumber;
+            TripSegContainerNumber = tripSegContainerNumber;
+            SubTitle = $"Trip {TripNumber}";
+        }
+
+        public override void Start()
+        {
+            Task.Run(async () =>
+            {
+                var containersTrip = await _tripSegmentRepository.AsQueryable()
+                    .Where(ts => ts.TripNumber == TripNumber).ToListAsync();
+                var containerSegments = await _tripSegmentContainerRepository.AsQueryable()
+                    .Where(
+                        tsc =>
+                            tsc.TripNumber == TripNumber && tsc.TripSegNumber == TripSegNumber &&
+                            tsc.TripSegContainerNumber == TripSegContainerNumber).ToListAsync();
+                var groupedContainers = from details in containerSegments
+                    orderby details.TripSegNumber
+                    group details by new {details.TripNumber, details.TripSegNumber}
+                    into detailsGroup
+                    select new Grouping<TripSegmentModel, TripSegmentContainerModel>(containersTrip.Find(
+                        tsm =>
+                            (tsm.TripNumber + tsm.TripSegNumber).Equals(detailsGroup.Key.TripNumber +
+                                                                        detailsGroup.Key.TripSegNumber)
+                        ), detailsGroup);
+                if (containersTrip.Any())
+                {
+                    TransactionList =
+                        new ObservableCollection<Grouping<TripSegmentModel, TripSegmentContainerModel>>(
+                            groupedContainers);
+                }
+            });
+            base.Start();
+        }
+
+        // Listview bindings
+        public ObservableCollection<Grouping<TripSegmentModel, TripSegmentContainerModel>> TransactionList { get; private set; }
+
         // Field bindings
-        // @TODO : In the near future, we could have multiple containers for bulk processing
-        public ContainerMasterModel Container { get; private set; }
+        private string _tripNumber;
+        public string TripNumber
+        {
+            get { return _tripNumber; }
+            set { SetProperty(ref _tripNumber, value); }
+        }
+
+        private string _tripSegNumber;
+        public string TripSegNumber
+        {
+            get { return _tripSegNumber; }
+            set { SetProperty(ref _tripSegNumber, value); }
+        }
+
+        private string _tripSegContainerNumber;
+        public string TripSegContainerNumber
+        {
+            get { return _tripSegContainerNumber; }
+            set { SetProperty(ref _tripSegContainerNumber, value); }
+        }
 
         private string _grossTime;
         public string GrossTime
@@ -99,22 +171,6 @@
         public bool IsGrossWeightSet()
         {
             return !string.IsNullOrWhiteSpace(GrossTime);
-        }
-
-        // @TODO : Refactor using repositories
-        private ContainerMasterModel CreateDummyData()
-        {
-            return new ContainerMasterModel
-            {
-                ContainerNumber = "89999",
-                ContainerType = "LUGGER",
-                ContainerSize = "20",
-                ContainerLocation = "DOCK DOOR 14",
-                ContainerCustHostCode = "KAMAN450",
-                ContainerStatus = "C",
-                ContainerCommodityCode = "1234",
-                ContainerCommodityDesc = "#15 SHEARING IRON"
-            };
         }
     }
 }
