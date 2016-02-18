39205881beabd0431942f2afd3c192d1a94a7c64
jaywhitlow <jay.whitlow@bradyplc.com>
2016-02-18 12:41:43 -0500

    Changes to Route Detail screen that include navigation buttons, and initial list of containers for each segment

--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/ViewModels/RouteDetailViewModel.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/ViewModels/RouteDetailViewModel.cs
@@ -1,4 +1,8 @@
-﻿namespace Brady.ScrapRunner.Mobile.ViewModels
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Brady.ScrapRunner.Mobile.Helpers;
+
+namespace Brady.ScrapRunner.Mobile.ViewModels
 {
     using System.Collections.ObjectModel;
     using System.Linq;
@@ -13,18 +17,21 @@
     {
         private readonly IRepository<TripModel> _tripRepository;
         private readonly IRepository<TripSegmentContainerModel> _tripSegmentContainerRepository;
+        private readonly IRepository<TripSegmentModel> _tripSegmentRepository;
         private string _custHostCode;
 
         public RouteDetailViewModel(
-            IRepository<TripModel> tripRepository, 
+            IRepository<TripModel> tripRepository,
+            IRepository<TripSegmentModel> tripSegmentRepository,
             IRepository<TripSegmentContainerModel> tripSegmentContainerRepository)
         {
             _tripRepository = tripRepository;
+            _tripSegmentRepository = tripSegmentRepository;
             _tripSegmentContainerRepository = tripSegmentContainerRepository;
             DirectionsCommand = new MvxCommand(ExecuteDrivingDirectionsCommand);
             EnRouteCommand = new MvxCommand(ExecuteEnRouteCommand);
             ArriveCommand = new MvxCommand(ExecuteArriveCommand);
-            TransactionCommand = new MvxCommand(ExecuteTransactionCommand);
+            NextStageCommand = new MvxCommand(ExecuteNextStageCommand);
         }
 
         public void Init(string tripNumber)
@@ -32,26 +39,42 @@
             TripNumber = tripNumber;
         }
 
-        public override async void Start()
+        public override void Start()
         {
-            base.Start();
-            var trip = await _tripRepository.FindAsync(t => t.TripNumber == TripNumber);
-            if (trip != null)
+            Task.Run(async () =>
             {
-                _custHostCode = trip.TripCustHostCode;
-                Title = trip.TripTypeDesc;
-                TripNumber = $"Trip {trip.TripNumber}";
-                TripCustName = trip.TripCustName;
-                TripDriverInstructions = trip.TripDriverInstructions;
-                TripCustAddress = trip.TripCustAddress1 + trip.TripCustAddress2;
-                TripCustCityStateZip = $"{trip.TripCustCity}, {trip.TripCustState} {trip.TripCustZip}";
-            }
+                var trip = await _tripRepository.FindAsync(t => t.TripNumber == TripNumber);
 
-            var containers = await _tripSegmentContainerRepository.ToListAsync(tsc => tsc.TripNumber == TripNumber);
-            if (containers.Any())
-            {
-                Containers = new ObservableCollection<TripSegmentContainerModel>(containers);
-            }
+                if (trip != null)
+                {
+                    _custHostCode = trip.TripCustHostCode;
+                    Title = trip.TripTypeDesc;
+                    SubTitle = $"Trip {trip.TripNumber}";
+                    TripCustName = trip.TripCustName;
+                    TripDriverInstructions = trip.TripDriverInstructions;
+                    TripCustAddress = trip.TripCustAddress1 + trip.TripCustAddress2;
+                    TripCustCityStateZip = $"{trip.TripCustCity}, {trip.TripCustState} {trip.TripCustZip}";
+                    TripType = trip.TripType;
+
+                    var containersTrip = await _tripSegmentRepository.AsQueryable()
+                        .Where(ts => ts.TripNumber == TripNumber).ToListAsync();
+                    var containerSegments = await _tripSegmentContainerRepository.AsQueryable()
+                        .Where(tsc => tsc.TripNumber == TripNumber).ToListAsync();
+                    var groupedContainers = from details in containerSegments
+                                            orderby details.TripSegNumber
+                                            group details by new { details.TripNumber, details.TripSegNumber }
+                                            into detailsGroup
+                                            select new Grouping<TripSegmentModel, TripSegmentContainerModel>(containersTrip.Find(
+                                                    tsm => (tsm.TripNumber + tsm.TripSegNumber).Equals(detailsGroup.Key.TripNumber + detailsGroup.Key.TripSegNumber)
+                                                ), detailsGroup);
+                    if (containersTrip.Any())
+                    {
+                        Containers = new ObservableCollection<Grouping<TripSegmentModel, TripSegmentContainerModel>>(groupedContainers);
+                    }
+                }
+            });
+
+            base.Start();
         }
 
         // Field bindings
@@ -60,6 +83,13 @@
         {
             get { return _tripNumber; }
             set { SetProperty(ref _tripNumber, value); }
+        }
+
+        private string _tripType;
+        public string TripType
+        {
+            get { return _tripType; }
+            set { SetProperty(ref _tripType, value); }
         }
 
         private string _tripDriverInstructions;
@@ -91,15 +121,14 @@
         }
 
         private string _currentStatus;
-
         public string CurrentStatus
         {
             get { return _currentStatus; }
             set { SetProperty(ref _currentStatus, value); }
         }
 
-        private ObservableCollection<TripSegmentContainerModel> _containers; 
-        public ObservableCollection<TripSegmentContainerModel> Containers
+        private ObservableCollection<Grouping<TripSegmentModel, TripSegmentContainerModel>> _containers; 
+        public ObservableCollection<Grouping<TripSegmentModel, TripSegmentContainerModel>> Containers
         {
             get { return _containers; }
             set { SetProperty(ref _containers, value); }
@@ -109,7 +138,7 @@
         public MvxCommand DirectionsCommand { get; private set; }
         public MvxCommand EnRouteCommand { get; private set; }
         public MvxCommand ArriveCommand { get; private set; }
-        public MvxCommand TransactionCommand { get; private set; }
+        public MvxCommand NextStageCommand { get; private set; }
 
         // Command impl
         private void ExecuteDrivingDirectionsCommand()
@@ -145,12 +174,23 @@
             if (confirm)
             {
                 CurrentStatus = "AR"; //DriverStatusConstants.Arrive;
+                //@TODO : REMOVE
+                ExecuteNextStageCommand();
             }
         }
 
-        private void ExecuteTransactionCommand()
+        private void ExecuteNextStageCommand()
         {
-            ShowViewModel<TransactionSummaryViewModel>();
+            if (TripType.Equals("SW"))
+            {
+                Close(this);
+                ShowViewModel<TransactionSummaryViewModel>(new { tripNumber = TripNumber });
+            }
+            else if (TripType.Equals("RT"))
+            {
+                Close(this);
+                ShowViewModel<ScaleSummaryViewModel>(new { tripNumber = TripNumber });
+            }
         }
     }
 }
