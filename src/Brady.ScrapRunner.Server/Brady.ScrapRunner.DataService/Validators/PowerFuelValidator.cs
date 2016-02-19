4700c5be98c7faaecb66390edd5a580f220f8073
jcherinka <janet.cherinka@bradyplc.com>
2016-02-19 15:29:53 -0500

    Added PowerFuel and DriverEfficiency

---
+++ src/Brady.ScrapRunner.Server/Brady.ScrapRunner.DataService/Validators/PowerFuelValidator.cs
@@ -0,0 +1,24 @@
+﻿using BWF.DataServices.Support.NHibernate.Interfaces;
+using FluentValidation;
+using Brady.ScrapRunner.Domain.Models;
+
+namespace Brady.ScrapRunner.DataService.Validators
+{
+    public class PowerFuelValidator :
+        AbstractValidator<PowerFuel>, IRequireCrudingDataServiceRepository
+    {
+        ICrudingDataServiceRepository _repository;
+
+        public PowerFuelValidator()
+        {
+            RuleFor(x => x.PowerFuelSeqNumber).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.PowerId).NotEmpty();
+            RuleFor(x => x.TripNumber).NotEmpty();
+        }
+
+        public void SetRepository(ICrudingDataServiceRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
