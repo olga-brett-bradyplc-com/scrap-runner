9dc9cfcb6c2b34f11166f2c841d97e5e1f1cee31
jcherinka <janet.cherinka@bradyplc.com>
2016-02-26 15:50:50 -0500

    Added 5 services

---
+++ src/Brady.ScrapRunner.Domain/Metadata/TripSegmentTimeMetadata.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brady.ScrapRunner.Domain.Models;
+using BWF.DataServices.Metadata.Fluent.Abstract;
+
+
+namespace Brady.ScrapRunner.Domain.Metadata
+{
+ 
+    public class TripSegmentTimeMetadata : TypeMetadataProvider<TripSegmentTime>
+    {
+        public TripSegmentTimeMetadata()
+        {
+            AutoUpdatesByDefault();
+
+            StringProperty(x => x.Id)
+                .IsHiddenInEditor()
+                .IsNotEditableInGrid();
+
+            StringProperty(x => x.TripNumber)
+                .IsId()
+                .DisplayName("Trip Number");
+
+            StringProperty(x => x.TripSegNumber)
+                .IsId()
+                .DisplayName("Trip Seg Number");
+
+            IntegerProperty(x => x.SeqNumber)
+                .IsId()
+                .DisplayName("Seq Number");
+
+            StringProperty(x => x.TimeType);
+            StringProperty(x => x.TimeDesc);
+            IntegerProperty(x => x.SegmentTime);
+
+            ViewDefaults()
+                .Property(x => x.TripNumber)
+                .Property(x => x.TripSegNumber)
+                .Property(x => x.SeqNumber)
+                .Property(x => x.TimeType)
+                .Property(x => x.TimeDesc)
+                .Property(x => x.SegmentTime)
+
+                .OrderBy(x => x.TripNumber)
+                .OrderBy(x => x.TripSegNumber)
+                .OrderBy(x => x.SeqNumber);
+
+        }
+    }
+}
