67fb64182573a55e057fe8cc1793e829f483033b
jaywhitlow <jay.whitlow@bradyplc.com>
2016-02-27 09:48:56 -0500

    Adding todo's

--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/ConnectionService.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/ConnectionService.cs
@@ -9,12 +9,13 @@ namespace Brady.ScrapRunner.Mobile.Services
     public class ConnectionService : IConnectionService<DataServiceClient>
     {
         private IDataServiceClient Connection { get; set; }
+
+        // @TODO : Set up configuration files/PCL to handle this
         private string HostName => "https://maunb-jtw10.bradyplc.com:7776";
 
         public bool CreateConnection(string username, string password, string dataService = null)
         {
-            // @TODO : Hardcoding this for now against the Brady Membership tables, while authing
-            // @TODO : against a valid EmployeeMaster record
+            // @TODO : Hardcoding BWF username/password, then we check for a valid EmployeeMaster record at login
             Connection = new DataServiceClient(HostName, "admin", "mem_2014", dataService);
             return true;
         }
@@ -30,6 +31,7 @@ namespace Brady.ScrapRunner.Mobile.Services
 
         public DataServiceClient GetConnection()
         {
+            // @TODO : Add validations to check to make sure a valid connection has been made, etc.
             return (DataServiceClient) Connection;
         }
     }
