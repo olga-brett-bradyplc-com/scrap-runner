aadb153a670093245f28794901cff27fa8e9fb4c
Trevor Balcom <trevorbalcom@me.com>
2016-02-28 09:17:26 -0500

    Adds Test project to Brady.ScrapRunner.Mobile solution. Implements AutoMapper Profile test.

---
+++ src/Brady.ScrapRunner.Mobile.Tests/ScrapRunnerProfileTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Brady.ScrapRunner.Mobile.Tests
+{
+    using AutoMapper;
+
+    [TestClass]
+    public class ScrapRunnerProfileTest
+    {
+        [TestMethod]
+        public void AutoMapper_Configuration_IsValid()
+        {
+            Mapper.Initialize(m => m.AddProfile<ScrapRunnerMapperProfile>());
+            Mapper.AssertConfigurationIsValid();
+        }
+    }
+}
