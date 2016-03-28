2bc693484da9a49a9a31b42258f67ce7130bdd6f
jcherinka <janet.cherinka@bradyplc.com>
2016-03-28 13:48:34 -0400

    Added ContainerChangeProcess

---
+++ src/Brady.ScrapRunner.Server/Brady.ScrapRunner.DataService/Validators/ContainerChangeProcessValidator.cs
@@ -0,0 +1,23 @@
+﻿using BWF.DataServices.Support.NHibernate.Interfaces;
+using FluentValidation;
+using Brady.ScrapRunner.Domain.Process;
+
+
+namespace Brady.ScrapRunner.DataService.Validators
+{
+    public class ContainerChangeProcessValidator :
+         AbstractValidator<ContainerChangeProcess>,
+         IRequireCrudingDataServiceRepository
+    {
+        ICrudingDataServiceRepository _repository;
+        public void SetRepository(ICrudingDataServiceRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ContainerChangeProcessValidator()
+        {
+            RuleFor(x => x.EmployeeId).NotEmpty();
+        }
+    }
+}
