471acbd8fb896865b0f3289749e46c81bf644196
Trevor Balcom <trevorbalcom@me.com>
2016-04-18 22:44:19 -0400

    Initial commit of LocationService.

---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/LocationService.cs
@@ -0,0 +1,55 @@
+﻿namespace Brady.ScrapRunner.Mobile.Services
+{
+    using System;
+    using Interfaces;
+    using MvvmCross.Platform;
+    using MvvmCross.Platform.Core;
+    using MvvmCross.Plugins.Location;
+
+    public class LocationService : ILocationService
+    {
+        private readonly IMvxLocationWatcher _locationWatcher;
+
+        public LocationService(IMvxLocationWatcher locationWatcher)
+        {
+            _locationWatcher = locationWatcher;
+            _locationWatcher.OnPermissionChanged += OnPermissionChange;
+        }
+
+        public void Start()
+        {
+            if (_locationWatcher.Started) return;
+            var options = new MvxLocationOptions
+            {
+                Accuracy = MvxLocationAccuracy.Fine,
+                TimeBetweenUpdates = new TimeSpan(0, 0, 0, 5),
+                TrackingMode = MvxLocationTrackingMode.Background
+            };
+
+            Mvx.Resolve<IMvxMainThreadDispatcher>().RequestMainThreadAction(() =>
+            {
+                _locationWatcher.Start(options, OnLocationChange, OnLocationError);
+            });
+        }
+
+        public void Stop()
+        {
+            if (!_locationWatcher.Started) return;
+            _locationWatcher.Stop();
+        }
+
+        private void OnLocationChange(MvxGeoLocation location)
+        {
+        }
+
+        private void OnLocationError(MvxLocationError locationError)
+        {
+            Mvx.Error($"Location error {locationError.Code}.");
+        }
+
+        private void OnPermissionChange(object sender, MvxValueEventArgs<MvxLocationPermission> mvxValueEventArgs)
+        {
+            Mvx.Warning($"Location permission {mvxValueEventArgs.Value}.");
+        }
+    }
+}
