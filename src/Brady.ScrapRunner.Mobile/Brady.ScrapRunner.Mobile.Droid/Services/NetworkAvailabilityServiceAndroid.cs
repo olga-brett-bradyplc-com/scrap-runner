e4b117d179fd3704fc2f5f85538f14919bc2e1d8
Trevor Balcom <trevorbalcom@me.com>
2016-04-25 23:30:17 -0400

    Refactors NetworkAvailabilityServiceAndroid.

--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile.Droid/Services/NetworkAvailabilityServiceAndroid.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile.Droid/Services/NetworkAvailabilityServiceAndroid.cs
@@ -1,23 +1,14 @@
 namespace Brady.ScrapRunner.Mobile.Droid.Services
 {
-    using Android.Content;
+    using Android.App;
     using Android.Net;
     using Interfaces;
-    using MvvmCross.Platform;
-    using MvvmCross.Platform.Droid;
 
     public class NetworkAvailabilityServiceAndroid : INetworkAvailabilityService
     {
         public bool IsNetworkConnectionAvailable()
         {
-            var androidGlobals = Mvx.Resolve<IMvxAndroidGlobals>();
-            if (androidGlobals == null)
-            {
-                Mvx.Warning("Cannnot determine if a network connection is available. Unable to Resolve type IMvxAndroidGlobals.");
-                return false;
-            }
-            var context = androidGlobals.ApplicationContext;
-            var cm = (ConnectivityManager)context.GetSystemService(Context.ConnectivityService);
+            var cm = ConnectivityManager.FromContext(Application.Context);
             var info = cm.ActiveNetworkInfo; // Requires manifest permission ACCESS_NETWORK_STATE.
             if (info == null) return false;
             var infoState = info.GetState();
