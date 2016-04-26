b50fcbdbf40f559019931137d161ee043609b4ef
Trevor Balcom <trevorbalcom@me.com>
2016-04-26 09:49:14 -0400

    Implementing OfflineCapableDataServiceClient.

---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/OfflineCapableDataServiceClient.cs
@@ -0,0 +1,131 @@
+﻿namespace Brady.ScrapRunner.Mobile.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using BWF.DataServices.Domain.Models;
+    using BWF.DataServices.Metadata.Interfaces;
+    using BWF.DataServices.Metadata.Models;
+    using BWF.DataServices.PortableClients;
+    using BWF.DataServices.PortableClients.Interfaces;
+    using Interfaces;
+    using Models;
+    using MvvmCross.Platform.Platform;
+
+    public class OfflineCapableDataServiceClient : IDataServiceClient
+    {
+        private readonly IConnectionService<IDataServiceClient> _connectionService;
+        private readonly INetworkAvailabilityService _networkAvailabilityService;
+        private readonly IRepository<QueueItemModel> _queueItemRepository;
+        private readonly IMvxJsonConverter _jsonConverter;
+
+        public OfflineCapableDataServiceClient(
+            IConnectionService<IDataServiceClient> connectionService, 
+            INetworkAvailabilityService networkAvailabilityService, 
+            IRepository<QueueItemModel> queueItemRepository, 
+            IMvxJsonConverter jsonConverter)
+        {
+            _connectionService = connectionService;
+            _networkAvailabilityService = networkAvailabilityService;
+            _queueItemRepository = queueItemRepository;
+            _jsonConverter = jsonConverter;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public Task<LiveQueryInstance> StartLiveQueryAsync(string query, Action<IEnumerable<RecordChangeDetails>, int> onChange, string dataService = null)
+        {
+            return _connectionService.GetConnection().StartLiveQueryAsync(query, onChange, dataService);
+        }
+
+        public Task<LiveQueryInstance<T>> StartLiveQueryAsync<T>(QueryBuilder<T> queryBuilder, Action<IEnumerable<RecordChangeDetails>, int> onChange, string dataService = null)
+        {
+            return _connectionService.GetConnection().StartLiveQueryAsync(queryBuilder, onChange, dataService);
+        }
+
+        public Task KillLiveQueryAsync(string queryId)
+        {
+            return _connectionService.GetConnection().KillLiveQueryAsync(queryId);
+        }
+
+        public Task<ChangeResultWithItem<T>> CreateAsync<T>(T item, string dataService = null, bool requeryCreated = true)
+        {
+            // @TODO: Add Offline support
+            return _connectionService.GetConnection().CreateAsync(item, dataService, requeryCreated);
+        }
+
+        public async Task<ChangeResultWithItem<T>> UpdateAsync<T>(T item, string dataService = null, bool requeryUpdated = true)
+        {
+            if (_networkAvailabilityService.IsNetworkConnectionAvailable())
+            {
+                // @TODO: Don't return if call fails for recoverable reason.
+                var response = await _connectionService.GetConnection().UpdateAsync(item, dataService, requeryUpdated);
+                return response;
+            }
+            var queueItem = GetQueueItemModel(item, QueueItemVerb.Delete);
+            await _queueItemRepository.InsertAsync(queueItem);
+            return new ChangeResultWithItem<T>();
+        }
+
+        public Task<ChangeResult> DeleteAsync<Tid, Titem>(Tid id, string dataService = null)
+        {
+            // @TODO: Add Offline support
+            return _connectionService.GetConnection().DeleteAsync<Tid, Titem>(id, dataService);
+        }
+
+        public Task<ChangeResult> DeleteAsync<Titem>(long id, string dataService = null)
+        {
+            // @TODO: Add Offline support
+            return _connectionService.GetConnection().DeleteAsync<Titem>(id, dataService);
+        }
+
+        public Task<ChangeResult> DeleteAsync<Titem>(int id, string dataService = null)
+        {
+            // @TODO: Add Offline support
+            return _connectionService.GetConnection().DeleteAsync<Titem>(id, dataService);
+        }
+
+        public Task<ChangeSetResult<Tid>> ProcessChangeSetAsync<Tid, Titem>(ChangeSet<Tid, Titem> changeSet, string dataService = null) where Titem : IHaveId<Tid>
+        {
+            return _connectionService.GetConnection().ProcessChangeSetAsync(changeSet, dataService);
+        }
+
+        public Task<Titem> GetAsync<Tid, Titem>(Tid id, string dataService = null)
+        {
+            return _connectionService.GetConnection().GetAsync<Tid, Titem>(id, dataService);
+        }
+
+        public Task<Titem> GetAsync<Titem>(long id, string dataService = null)
+        {
+            return _connectionService.GetConnection().GetAsync<Titem>(id, dataService);
+        }
+
+        public Task<Titem> GetAsync<Titem>(int id, string dataService = null)
+        {
+            return _connectionService.GetConnection().GetAsync<Titem>(id, dataService);
+        }
+
+        public Task<QueryResult> QueryAsync(string query, string dataService = null)
+        {
+            return _connectionService.GetConnection().QueryAsync(query, dataService);
+        }
+
+        public Task<QueryResult<T>> QueryAsync<T>(QueryBuilder<T> queryBuilder, string dataService = null)
+        {
+            return _connectionService.GetConnection().QueryAsync(queryBuilder, dataService);
+        }
+
+        private QueueItemModel GetQueueItemModel<T>(T obj, QueueItemVerb verb)
+        {
+            return new QueueItemModel
+            {
+                RecordType = obj.GetType().ToString(),
+                SerializedRecord = _jsonConverter.SerializeObject(obj),
+                Verb = verb
+            };
+
+        }
+    }
+}
