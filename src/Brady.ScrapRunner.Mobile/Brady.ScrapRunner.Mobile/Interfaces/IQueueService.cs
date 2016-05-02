c4004da7d662fab2ed3db8ea423bb88535e62adf
Trevor Balcom <trevorbalcom@me.com>
2016-05-02 15:55:20 -0400

    Implements QueueService.

--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Interfaces/IQueueService.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Interfaces/IQueueService.cs
@@ -5,6 +5,6 @@
     public interface IQueueService
     {
         Task ProcessQueueAsync();
-        bool IsEmpty { get; }
+        Task<bool> IsEmptyAsync();
     }
 }
---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/QueueService.cs
@@ -0,0 +1,26 @@
+﻿namespace Brady.ScrapRunner.Mobile.Services
+{
+    using System.Threading.Tasks;
+    using Interfaces;
+    using Models;
+
+    public class QueueService : IQueueService
+    {
+        private readonly IRepository<QueueItemModel> _repository;
+
+        public QueueService(IRepository<QueueItemModel> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task ProcessQueueAsync()
+        {
+            await Task.Delay(0); // Async noop
+        }
+
+        public async Task<bool> IsEmptyAsync()
+        {
+            return await _repository.AsQueryable().CountAsync() > 0;
+        }
+    }
+}
