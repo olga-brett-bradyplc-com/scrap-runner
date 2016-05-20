8b619f13e66ca232539c4ab6dfd8dea80dd77d2c
jaywhitlow <jay.whitlow@bradyplc.com>
2016-05-20 11:43:57 -0400

    New methods for container service that allow us to modify various container properties

--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Interfaces/IContainerService.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Interfaces/IContainerService.cs
@@ -11,6 +11,9 @@ namespace Brady.ScrapRunner.Mobile.Interfaces
     public interface IContainerService
     {
         Task UpdateContainerMaster(List<ContainerMaster> containerMaster);
-        Task<IEnumerable<ContainerMasterModel>> FindPowerIdContainers(string powerId);
+        Task<IEnumerable<ContainerMasterModel>> FindPowerIdContainersAsync(string powerId);
+        Task<ContainerMasterModel> FindContainerAsync(string containerNumber);
+        Task<int> UpdateNbContainerAsync(string containerNumber);
+        Task<int> RemoveContainerFromPowerId(string powerId, string containerNumber);
     }
 }
--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/ContainerService.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/ContainerService.cs
@@ -13,12 +13,10 @@ namespace Brady.ScrapRunner.Mobile.Services
     public class ContainerService : IContainerService
     {
         private readonly IRepository<ContainerMasterModel> _containerRepository;
-        private readonly IConnectionService<DataServiceClient> _connection;  
 
-        public ContainerService(IRepository<ContainerMasterModel> containerRepository, IConnectionService<DataServiceClient> connection )
+        public ContainerService(IRepository<ContainerMasterModel> containerRepository)
         {
             _containerRepository = containerRepository;
-            _connection = connection;
         } 
 
         public Task UpdateContainerMaster(List<ContainerMaster> containerMaster)
@@ -27,12 +25,31 @@ namespace Brady.ScrapRunner.Mobile.Services
             return _containerRepository.InsertRangeAsync(mapped);
         }
 
-        public async Task<IEnumerable<ContainerMasterModel>> FindPowerIdContainers(string powerId)
+        public async Task<ContainerMasterModel> FindContainerAsync(string containerNumber)
+        {
+            return await _containerRepository.FindAsync(ct => ct.ContainerNumber == containerNumber);
+        }
+
+        public async Task<IEnumerable<ContainerMasterModel>> FindPowerIdContainersAsync(string powerId)
         {
             var containers = await _containerRepository.AsQueryable()
                 .Where(ct => ct.ContainerPowerId == powerId).ToListAsync();
 
             return containers;
+        }
+
+        public async Task<int> UpdateNbContainerAsync(string containerNumber)
+        {
+            var container = await FindContainerAsync(containerNumber);
+            container.ContainerType = "LUGR";
+            return await _containerRepository.UpdateAsync(container);
+        }
+
+        public async Task<int> RemoveContainerFromPowerId(string powerId, string containerNumber)
+        {
+            var container = await FindContainerAsync(containerNumber);
+            container.ContainerPowerId = null;
+            return await _containerRepository.UpdateAsync(container);
         }
     }
 }
