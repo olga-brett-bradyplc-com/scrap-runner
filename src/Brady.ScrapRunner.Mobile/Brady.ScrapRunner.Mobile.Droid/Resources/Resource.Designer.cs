645e7fc9f9a2970933fa141d3e9156bc7cdc92ab
jaywhitlow <jay.whitlow@bradyplc.com>
2016-05-23 17:58:18 -0400

    WIP of messaging screens from Olga's initial pass. Still a little more styling work to be done

--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile.Droid/Resources/Resource.Designer.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile.Droid/Resources/Resource.Designer.cs
@@ -2772,32 +2772,32 @@ namespace Brady.ScrapRunner.Mobile.Droid
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f08006f
-			public const int abc_background_cache_hint_selector_material_dark = 2131230831;
-			
 			// aapt resource value: 0x7f080070
-			public const int abc_background_cache_hint_selector_material_light = 2131230832;
+			public const int abc_background_cache_hint_selector_material_dark = 2131230832;
 			
 			// aapt resource value: 0x7f080071
-			public const int abc_color_highlight_material = 2131230833;
+			public const int abc_background_cache_hint_selector_material_light = 2131230833;
+			
+			// aapt resource value: 0x7f080072
+			public const int abc_color_highlight_material = 2131230834;
 			
 			// aapt resource value: 0x7f080004
 			public const int abc_input_method_navigation_guard = 2131230724;
 			
-			// aapt resource value: 0x7f080072
-			public const int abc_primary_text_disable_only_material_dark = 2131230834;
-			
 			// aapt resource value: 0x7f080073
-			public const int abc_primary_text_disable_only_material_light = 2131230835;
+			public const int abc_primary_text_disable_only_material_dark = 2131230835;
 			
 			// aapt resource value: 0x7f080074
-			public const int abc_primary_text_material_dark = 2131230836;
+			public const int abc_primary_text_disable_only_material_light = 2131230836;
 			
 			// aapt resource value: 0x7f080075
-			public const int abc_primary_text_material_light = 2131230837;
+			public const int abc_primary_text_material_dark = 2131230837;
 			
 			// aapt resource value: 0x7f080076
-			public const int abc_search_url_text = 2131230838;
+			public const int abc_primary_text_material_light = 2131230838;
+			
+			// aapt resource value: 0x7f080077
+			public const int abc_search_url_text = 2131230839;
 			
 			// aapt resource value: 0x7f080005
 			public const int abc_search_url_text_normal = 2131230725;
@@ -2808,11 +2808,11 @@ namespace Brady.ScrapRunner.Mobile.Droid
 			// aapt resource value: 0x7f080007
 			public const int abc_search_url_text_selected = 2131230727;
 			
-			// aapt resource value: 0x7f080077
-			public const int abc_secondary_text_material_dark = 2131230839;
-			
 			// aapt resource value: 0x7f080078
-			public const int abc_secondary_text_material_light = 2131230840;
+			public const int abc_secondary_text_material_dark = 2131230840;
+			
+			// aapt resource value: 0x7f080079
+			public const int abc_secondary_text_material_light = 2131230841;
 			
 			// aapt resource value: 0x7f080058
 			public const int accent = 2131230808;
@@ -2957,6 +2957,9 @@ namespace Brady.ScrapRunner.Mobile.Droid
 			
 			// aapt resource value: 0x7f08001f
 			public const int hint_foreground_material_light = 2131230751;
+			
+			// aapt resource value: 0x7f08006f
+			public const int incoming_message = 2131230831;
 			
 			// aapt resource value: 0x7f08005a
 			public const int material_100 = 2131230810;
@@ -3129,11 +3132,11 @@ namespace Brady.ScrapRunner.Mobile.Droid
 			// aapt resource value: 0x7f08003b
 			public const int switch_thumb_disabled_material_light = 2131230779;
 			
-			// aapt resource value: 0x7f080079
-			public const int switch_thumb_material_dark = 2131230841;
-			
 			// aapt resource value: 0x7f08007a
-			public const int switch_thumb_material_light = 2131230842;
+			public const int switch_thumb_material_dark = 2131230842;
+			
+			// aapt resource value: 0x7f08007b
+			public const int switch_thumb_material_light = 2131230843;
 			
 			// aapt resource value: 0x7f08003c
 			public const int switch_thumb_normal_material_dark = 2131230780;
@@ -3756,190 +3759,193 @@ namespace Brady.ScrapRunner.Mobile.Droid
 			public const int abc_textfield_search_material = 2130837578;
 			
 			// aapt resource value: 0x7f02004b
-			public const int design_fab_background = 2130837579;
+			public const int chatbubble = 2130837579;
 			
 			// aapt resource value: 0x7f02004c
-			public const int design_snackbar_background = 2130837580;
+			public const int design_fab_background = 2130837580;
 			
 			// aapt resource value: 0x7f02004d
-			public const int drawericon = 2130837581;
+			public const int design_snackbar_background = 2130837581;
 			
 			// aapt resource value: 0x7f02004e
-			public const int drawericon2 = 2130837582;
+			public const int drawericon = 2130837582;
 			
 			// aapt resource value: 0x7f02004f
-			public const int ic_add_a_photo_black_36dp = 2130837583;
+			public const int drawericon2 = 2130837583;
 			
 			// aapt resource value: 0x7f020050
-			public const int ic_add_white_36dp = 2130837584;
+			public const int ic_add_a_photo_black_36dp = 2130837584;
 			
 			// aapt resource value: 0x7f020051
-			public const int ic_alarm_on_black_24dp = 2130837585;
+			public const int ic_add_white_36dp = 2130837585;
 			
 			// aapt resource value: 0x7f020052
-			public const int ic_alarm_on_white_24dp = 2130837586;
+			public const int ic_alarm_on_black_24dp = 2130837586;
 			
 			// aapt resource value: 0x7f020053
-			public const int ic_assignment_black_36dp = 2130837587;
+			public const int ic_alarm_on_white_24dp = 2130837587;
 			
 			// aapt resource value: 0x7f020054
-			public const int ic_assignment_late_white_24dp = 2130837588;
+			public const int ic_assignment_black_36dp = 2130837588;
 			
 			// aapt resource value: 0x7f020055
-			public const int ic_assignment_turned_in_white_24dp = 2130837589;
+			public const int ic_assignment_late_white_24dp = 2130837589;
 			
 			// aapt resource value: 0x7f020056
-			public const int ic_block_black_36dp = 2130837590;
+			public const int ic_assignment_turned_in_white_24dp = 2130837590;
 			
 			// aapt resource value: 0x7f020057
-			public const int ic_chat_black_36dp = 2130837591;
+			public const int ic_block_black_36dp = 2130837591;
 			
 			// aapt resource value: 0x7f020058
-			public const int ic_check_circle_green_36dp = 2130837592;
+			public const int ic_chat_black_36dp = 2130837592;
 			
 			// aapt resource value: 0x7f020059
-			public const int ic_delete_sweep_white_36dp = 2130837593;
+			public const int ic_check_circle_green_36dp = 2130837593;
 			
 			// aapt resource value: 0x7f02005a
-			public const int ic_directions_bus_black_36dp = 2130837594;
+			public const int ic_delete_sweep_white_36dp = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int ic_directions_subway_black_36dp = 2130837595;
+			public const int ic_directions_bus_black_36dp = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int ic_directions_white_36dp = 2130837596;
+			public const int ic_directions_subway_black_36dp = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int ic_email_black_36dp = 2130837597;
+			public const int ic_directions_white_36dp = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int ic_errorstatus = 2130837598;
+			public const int ic_email_black_36dp = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int ic_forum_black_36dp = 2130837599;
+			public const int ic_errorstatus = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int ic_help_black_36dp = 2130837600;
+			public const int ic_forum_black_36dp = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int ic_info_black_36dp = 2130837601;
+			public const int ic_help_black_36dp = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int ic_keyboard_arrow_right_black_24dp = 2130837602;
+			public const int ic_info_black_36dp = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int ic_local_gas_station_black_36dp = 2130837603;
+			public const int ic_keyboard_arrow_right_black_24dp = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int ic_local_shipping_white_18dp = 2130837604;
+			public const int ic_local_gas_station_black_36dp = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int ic_local_shipping_white_24dp = 2130837605;
+			public const int ic_local_shipping_white_18dp = 2130837605;
 			
 			// aapt resource value: 0x7f020066
-			public const int ic_local_shipping_white_36dp = 2130837606;
+			public const int ic_local_shipping_white_24dp = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int ic_loupe_white_36dp = 2130837607;
+			public const int ic_local_shipping_white_36dp = 2130837607;
 			
 			// aapt resource value: 0x7f020068
-			public const int ic_phonelink_erase_black_36dp = 2130837608;
+			public const int ic_loupe_white_36dp = 2130837608;
 			
 			// aapt resource value: 0x7f020069
-			public const int ic_phonelink_setup_black_36dp = 2130837609;
+			public const int ic_phonelink_erase_black_36dp = 2130837609;
 			
 			// aapt resource value: 0x7f02006a
-			public const int ic_place_white_36dp = 2130837610;
+			public const int ic_phonelink_setup_black_36dp = 2130837610;
 			
 			// aapt resource value: 0x7f02006b
-			public const int ic_queue_black_36dp = 2130837611;
+			public const int ic_place_white_36dp = 2130837611;
 			
 			// aapt resource value: 0x7f02006c
-			public const int ic_redo_black_36dp = 2130837612;
+			public const int ic_queue_black_36dp = 2130837612;
 			
 			// aapt resource value: 0x7f02006d
-			public const int ic_report_problem_black_36dp = 2130837613;
+			public const int ic_redo_black_36dp = 2130837613;
 			
 			// aapt resource value: 0x7f02006e
-			public const int ic_settings_white_36dp = 2130837614;
+			public const int ic_report_problem_black_36dp = 2130837614;
 			
 			// aapt resource value: 0x7f02006f
-			public const int ic_settings_white_48dp = 2130837615;
+			public const int ic_settings_white_36dp = 2130837615;
 			
 			// aapt resource value: 0x7f020070
-			public const int ic_successstatus = 2130837616;
+			public const int ic_settings_white_48dp = 2130837616;
 			
 			// aapt resource value: 0x7f020071
-			public const int ic_swap_vert_black_36dp = 2130837617;
+			public const int ic_successstatus = 2130837617;
 			
 			// aapt resource value: 0x7f020072
-			public const int ic_view_quilt_black_36dp = 2130837618;
+			public const int ic_swap_vert_black_36dp = 2130837618;
 			
 			// aapt resource value: 0x7f020073
-			public const int icon = 2130837619;
+			public const int ic_view_quilt_black_36dp = 2130837619;
 			
 			// aapt resource value: 0x7f020074
-			public const int loadingxamarin = 2130837620;
+			public const int icon = 2130837620;
 			
 			// aapt resource value: 0x7f020075
-			public const int loginsplash800x1280 = 2130837621;
+			public const int loadingxamarin = 2130837621;
 			
 			// aapt resource value: 0x7f020076
-			public const int loginsplash800x1280_4 = 2130837622;
+			public const int loginsplash800x1280 = 2130837622;
 			
 			// aapt resource value: 0x7f020077
-			public const int logo = 2130837623;
+			public const int loginsplash800x1280_4 = 2130837623;
 			
 			// aapt resource value: 0x7f020078
-			public const int logo_2 = 2130837624;
+			public const int logo = 2130837624;
 			
 			// aapt resource value: 0x7f020079
-			public const int navigation_header_background = 2130837625;
-			
-			// aapt resource value: 0x7f020088
-			public const int notification_template_icon_bg = 2130837640;
+			public const int logo_2 = 2130837625;
 			
 			// aapt resource value: 0x7f02007a
-			public const int rectangle_border = 2130837626;
+			public const int navigation_header_background = 2130837626;
+			
+			// aapt resource value: 0x7f020089
+			public const int notification_template_icon_bg = 2130837641;
 			
 			// aapt resource value: 0x7f02007b
-			public const int round_button_arrive = 2130837627;
+			public const int rectangle_border = 2130837627;
 			
 			// aapt resource value: 0x7f02007c
-			public const int round_button_directions = 2130837628;
+			public const int round_button_arrive = 2130837628;
 			
 			// aapt resource value: 0x7f02007d
-			public const int round_button_enroute = 2130837629;
+			public const int round_button_directions = 2130837629;
 			
 			// aapt resource value: 0x7f02007e
-			public const int rounded_edittext_bottom = 2130837630;
+			public const int round_button_enroute = 2130837630;
 			
 			// aapt resource value: 0x7f02007f
-			public const int rounded_edittext_bottom_focused = 2130837631;
+			public const int rounded_edittext_bottom = 2130837631;
 			
 			// aapt resource value: 0x7f020080
-			public const int rounded_edittext_bottom_states = 2130837632;
+			public const int rounded_edittext_bottom_focused = 2130837632;
 			
 			// aapt resource value: 0x7f020081
-			public const int rounded_edittext_top = 2130837633;
+			public const int rounded_edittext_bottom_states = 2130837633;
 			
 			// aapt resource value: 0x7f020082
-			public const int rounded_edittext_top_focused = 2130837634;
+			public const int rounded_edittext_top = 2130837634;
 			
 			// aapt resource value: 0x7f020083
-			public const int rounded_edittext_top_states = 2130837635;
+			public const int rounded_edittext_top_focused = 2130837635;
 			
 			// aapt resource value: 0x7f020084
-			public const int roundedbg = 2130837636;
+			public const int rounded_edittext_top_states = 2130837636;
 			
 			// aapt resource value: 0x7f020085
-			public const int roundedbgdark = 2130837637;
+			public const int roundedbg = 2130837637;
 			
 			// aapt resource value: 0x7f020086
-			public const int splash = 2130837638;
+			public const int roundedbgdark = 2130837638;
 			
 			// aapt resource value: 0x7f020087
-			public const int user_icon_header_navigation = 2130837639;
+			public const int splash = 2130837639;
+			
+			// aapt resource value: 0x7f020088
+			public const int user_icon_header_navigation = 2130837640;
 			
 			static Drawable()
 			{
--- src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/ViewModels/MessagesViewModel.cs
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/ViewModels/MessagesViewModel.cs
@@ -1,26 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Acr.UserDialogs;
-using Brady.ScrapRunner.Domain;
-using Brady.ScrapRunner.Domain.Models;
-using Brady.ScrapRunner.Domain.Process;
-using Brady.ScrapRunner.Mobile.Interfaces;
-using Brady.ScrapRunner.Mobile.Models;
-using Brady.ScrapRunner.Mobile.Services;
-using Brady.ScrapRunner.Mobile.Validators;
-using BWF.DataServices.Metadata.Fluent.Utils;
-using BWF.DataServices.PortableClients;
-using MvvmCross.Binding.BindingContext;
-using MvvmCross.Binding.ExtensionMethods;
-using MvvmCross.Localization;
-
-namespace Brady.ScrapRunner.Mobile.ViewModels
+﻿namespace Brady.ScrapRunner.Mobile.ViewModels
 {
+    using System.Linq;
+    using Brady.ScrapRunner.Mobile.Interfaces;
+    using Brady.ScrapRunner.Mobile.Models;
     using System.Collections.ObjectModel;
-    using System.Globalization;
     using MvvmCross.Core.ViewModels;
     using Resources;
 
@@ -36,43 +19,15 @@ namespace Brady.ScrapRunner.Mobile.ViewModels
             _messagesService = messagesService;
 
             Title = AppResources.Messages;
-            MessageSelectedCommand = new MvxCommand<MessagesModel>(ExecuteMessageSelectedCommand);
         }
+
         public override async void Start()
         {
             var currentDriver = await _driverService.GetCurrentDriverStatusAsync();
-
             var messages = await _messagesService.SortedDrvrMsgsAsync();
-            Messages = new ObservableCollection<MessagesModel>(messages);
+            var messagegroupedby = messages.GroupBy(m => m.SenderId).Select(grp => grp.First()).ToList();
 
-            if (Messages.Count == 0)
-            {
-                MessagesModel m1 = new MessagesModel();
-                m1.MsgText = "Terminal is closing";
-                m1.Ack = "N";
-                m1.CreateDateTime = DateTime.Now;
-                m1.MsgId = 1;
-                m1.ReceiverId = "930";
-                m1.SenderId = "Dispatcher #1";
-                m1.ReceiverName = "Steve Hartman";
-                m1.SenderName = "John Smith";
-
-                Messages.Add(m1);
-
-                MessagesModel m2 = new MessagesModel();
-                m2.MsgText = "Be carefule on icy road";
-                m2.Ack = "N";
-                m2.CreateDateTime = DateTime.Now;
-                m2.MsgId = 2;
-                m2.ReceiverId = "930";
-                m2.SenderId = "Dispatcher #2";
-                m2.ReceiverName = "Steve Hartman";
-                m2.SenderName = "Julian Northman";
-
-                Messages.Add(m2);
-
-            }
-
+            Messages = new ObservableCollection<MessagesModel>(messagegroupedby);
             base.Start();
         }
 
@@ -87,17 +42,15 @@ namespace Brady.ScrapRunner.Mobile.ViewModels
         public MessagesModel SelectedMessage
         {
             get { return _selectedMessage; }
-            set
-            {
-                _selectedMessage = value; RaisePropertyChanged(() => SelectedMessage);
-            }
+            set { SetProperty(ref _selectedMessage, value); }
         }
-        public MvxCommand<MessagesModel> MessageSelectedCommand { get; private set; }
 
-        public void ExecuteMessageSelectedCommand(MessagesModel selectedMessage)
+        private IMvxCommand _messageSelectedCommand;
+        public IMvxCommand MessageSelectedCommand => _messageSelectedCommand ?? (_messageSelectedCommand = new MvxCommand<MessagesModel>(ExecuteMessageSelectedCommand));
+
+        public void ExecuteMessageSelectedCommand(MessagesModel messageModel)
         {
-            Close(this);
-            ShowViewModel<NewMessageViewModel>(new { message = selectedMessage.SenderId });
+            ShowViewModel<NewMessageViewModel>(new { remoteUserId = messageModel.SenderId });
         }
     }
 }
