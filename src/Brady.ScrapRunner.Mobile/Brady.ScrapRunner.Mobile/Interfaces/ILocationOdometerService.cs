6611695110241bd3d5bdadb81ab02af287a441f9
Trevor Balcom <trevorbalcom@me.com>
2016-06-12 19:43:56 -0400

    Adds ILocationOdometerService.

---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Interfaces/ILocationOdometerService.cs
@@ -0,0 +1,10 @@
+﻿namespace Brady.ScrapRunner.Mobile.Interfaces
+{
+    public interface ILocationOdometerService
+    {
+        void Start(int startingOdometer);
+        void Stop();
+
+        int? CurrentOdometer { get; }
+    }
+}
---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Services/LocationOdometerService.cs
@@ -0,0 +1,43 @@
+﻿namespace Brady.ScrapRunner.Mobile.Services
+{
+    using System;
+    using Interfaces;
+    using Messages;
+    using MvvmCross.Plugins.Messenger;
+
+    public class LocationOdometerService : ILocationOdometerService
+    {
+        private readonly IMvxMessenger _mvxMessenger;
+        private MvxSubscriptionToken _mvxSubscriptionToken;
+        private int _startingOdometer = 0;
+        private uint _miles16; // .01 miles * 16
+        private uint _offlineGuess;
+        private int _latitude = 0;
+        private int _longitude = 0;
+        private DateTimeOffset _dateTime;
+        private DateTimeOffset _startingDateTime;
+
+        public LocationOdometerService(IMvxMessenger mvxMessenger)
+        {
+            _mvxMessenger = mvxMessenger;
+        }
+
+        public void Start(int startingOdometer)
+        {
+            _startingOdometer = startingOdometer;
+            _mvxSubscriptionToken = _mvxMessenger.Subscribe<LocationModelMessage>(OnLocationModelMessage);
+        }
+
+        public void Stop()
+        {
+            _mvxMessenger.Unsubscribe<LocationModelMessage>(_mvxSubscriptionToken);
+        }
+
+        public int? CurrentOdometer { get; }
+
+        private void OnLocationModelMessage(LocationModelMessage obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
