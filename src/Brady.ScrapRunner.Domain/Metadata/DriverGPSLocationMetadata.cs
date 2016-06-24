9b9bf4b8c46ee754458409f130b799a60639b519
jcherinka <janet.cherinka@bradyplc.com>
2016-06-24 14:17:56 -0400

    Added DriverGPSLocationProcess and GPSLocation

---
+++ src/Brady.ScrapRunner.Domain/Metadata/DriverGPSLocationMetadata.cs
@@ -0,0 +1,40 @@
+﻿using Brady.ScrapRunner.Domain.Process;
+using BWF.DataServices.Metadata.Fluent.Abstract;
+
+namespace Brady.ScrapRunner.Domain.Metadata
+{
+    public class DriverGPSLocationProcessMetadata : TypeMetadataProvider<DriverGPSLocationProcess>
+    {
+        public DriverGPSLocationProcessMetadata()
+        {
+
+            AutoUpdatesByDefault();
+
+            StringProperty(x => x.Id)
+                .IsHiddenInEditor()
+                .IsNotEditableInGrid();
+
+            StringProperty(x => x.EmployeeId)
+                .IsId()
+                .DisplayName("Employee Id");
+
+            IntegerProperty(x => x.GPSID);
+            TimeProperty(x => x.ActionDateTime);
+            IntegerProperty(x => x.Latitude);
+            IntegerProperty(x => x.Longitude);
+            IntegerProperty(x => x.Speed);
+            IntegerProperty(x => x.Heading);
+
+            ViewDefaults()
+            .Property(x => x.EmployeeId)
+            .Property(x => x.GPSID)
+            .Property(x => x.ActionDateTime)
+            .Property(x => x.Latitude)
+            .Property(x => x.Longitude)
+            .Property(x => x.Speed)
+            .Property(x => x.Heading)
+
+            .OrderBy(x => x.EmployeeId);
+        }
+    }
+}
