8bee8dde9debc8ad86962e5ceab9f24b3c863055
Trevor Balcom <trevorbalcom@me.com>
2016-07-11 20:21:14 -0400

    Implements NotificationModel & configures IRepository for NotificationModel type.

---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Models/NotificationModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Brady.ScrapRunner.Mobile.Models
+{
+    using System;
+    using SQLite.Net.Attributes;
+
+    [Table("Notifications")]
+    public class NotificationModel
+    {
+        [PrimaryKey, AutoIncrement]
+        public int? Id { get; set; }
+        public NotificationType NotificationType { get; set; }
+        public string Summary { get; set; }
+        public DateTimeOffset? NotificationDateTimeOffset { get; set; }
+    }
+
+    public enum NotificationType
+    {
+        NewTrip,
+        TripModified,
+        TripCanceled,
+        TripOnHold,
+        TripFuture,
+        TripReassigned,
+        TripUnassigned,
+        TripMarkedDone,
+        TripsResequenced,
+        NewMessage
+    }
+}
