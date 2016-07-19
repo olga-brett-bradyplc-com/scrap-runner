28f999bfa4bd4109274d342812598b6a0e3f0d50
Trevor Balcom <trevorbalcom@me.com>
2016-07-18 23:41:12 -0400

    Uses ValueConverters for a cleaner item_notification layout.

---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Converters/DateTimeOffsetValueConverter.cs
@@ -0,0 +1,16 @@
+﻿namespace Brady.ScrapRunner.Mobile.Converters
+{
+    using System;
+    using System.Globalization;
+    using MvvmCross.Platform.Converters;
+
+    public class DateTimeOffsetValueConverter : MvxValueConverter<DateTimeOffset, string>
+    {
+        protected override string Convert(DateTimeOffset value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter == null) return value.ToString("HH:mm", culture);
+            var format = parameter as string;
+            return value.ToString(format, culture);
+        }
+    }
+}
---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Converters/NotificationTypeValueConverter.cs
@@ -0,0 +1,40 @@
+﻿namespace Brady.ScrapRunner.Mobile.Converters
+{
+    using System;
+    using System.Globalization;
+    using Models;
+    using MvvmCross.Platform.Converters;
+    using Resources;
+
+    public class NotificationTypeValueConverter : MvxValueConverter<NotificationType, string>
+    {
+        protected override string Convert(NotificationType value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case NotificationType.NewTrip:
+                    return AppResources.NotificationNewTripTitle;
+                case NotificationType.TripModified:
+                    return AppResources.NotificationTripModifiedTitle;
+                case NotificationType.TripCanceled:
+                    return AppResources.NotificationTripCanceledTitle;
+                case NotificationType.TripOnHold:
+                    return AppResources.NotificationTripOnHoldTitle;
+                case NotificationType.TripFuture:
+                    return AppResources.NotificationTripFutureTitle;
+                case NotificationType.TripReassigned:
+                    return AppResources.NotificationTripReassignedTitle;
+                case NotificationType.TripUnassigned:
+                    return AppResources.NotificationTripUnassignedTitle;
+                case NotificationType.TripMarkedDone:
+                    return AppResources.NotificationTripMarkedDoneTitle;
+                case NotificationType.TripsResequenced:
+                    return AppResources.NotificationTripResequenceTitle;
+                case NotificationType.NewMessage:
+                    return AppResources.NotificationMessageTitle;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+        }
+    }
+}
