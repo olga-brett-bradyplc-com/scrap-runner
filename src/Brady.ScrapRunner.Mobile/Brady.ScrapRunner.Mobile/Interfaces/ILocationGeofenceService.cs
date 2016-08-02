eb04a7b0a115d1d76f7301bca2aad06d9068be39
Trevor Balcom <trevorbalcom@me.com>
2016-08-01 21:44:44 -0400

    Implements ILocationGeofenceService.

---
+++ src/Brady.ScrapRunner.Mobile/Brady.ScrapRunner.Mobile/Interfaces/ILocationGeofenceService.cs
@@ -0,0 +1,10 @@
+﻿namespace Brady.ScrapRunner.Mobile.Interfaces
+{
+    using System.Threading.Tasks;
+
+    public interface ILocationGeofenceService
+    {
+        void StartAutoArrive(string key, int synergyLatitude, int synergyLongitude, short radius);
+        void StartAutoDepart(string key);
+    }
+}
