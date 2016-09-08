7bb13d38fe88a2f11d5390630a33ac6fe5420707
jcherinka <janet.cherinka@bradyplc.com>
2016-09-08 08:01:39 -0400

    Changes to PreferencesProcess to allow for individual driver preferences.

---
+++ src/Brady.ScrapRunner.Server/Brady.ScrapRunner.DataService/Mappings/EmployeePreferencesMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brady.ScrapRunner.Domain.Models;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Brady.ScrapRunner.DataService.Mappings
+{
+    /// <summary>
+    /// An EmployeePreferences mapping to NHibernate.
+    /// </summary>
+    public class EmployeePreferencesMap : ClassMapping<EmployeePreferences>
+    {
+        public EmployeePreferencesMap()
+        {
+            Table("EmployeePreferences");
+
+            ComposedId(map =>
+            {
+                map.Property(y => y.RegionId, m => m.Generated(PropertyGeneration.Never));
+                map.Property(y => y.TerminalId, m => m.Generated(PropertyGeneration.Never));
+                map.Property(y => y.EmployeeId, m => m.Generated(PropertyGeneration.Never));
+                map.Property(y => y.Parameter, m => m.Generated(PropertyGeneration.Never));
+            });
+
+            Property(x => x.Id, m =>
+            {
+                m.Formula("concat(RegionId, ';', TerminalId, ';', EmployeeId, ';', Parameter)");
+                m.Insert(false);
+                m.Update(false);
+            });
+
+            Property(x => x.ParameterValue);
+            Property(x => x.Description);
+            Property(x => x.PreferenceType);
+            Property(x => x.PreferenceSeqNo);
+        }
+    }
+}
